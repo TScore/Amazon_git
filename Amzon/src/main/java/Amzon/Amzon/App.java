bfc612543dbf0bfba79acd525685ec8baa27bff2
tapanakumarbadhai <tapanakumarbadhai@192.168.0.100>
2019-04-10 12:15:17 +0530

    Amazoncomit

---
+++ Amzon/src/main/java/Amzon/Amzon/App.java
@@ -0,0 +1,13 @@
+package Amzon.Amzon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
